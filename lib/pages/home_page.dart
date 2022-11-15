9fe5806284f6015df361952829523304f4f1e9de
vishalkajale <vishal.kajale@ideastoimpacts.com>
2022-11-15 18:12:47 +0530

    #REFRACTOR

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geofence_flutter/geofence_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:positioned_tap_detector_2/positioned_tap_detector_2.dart';
@@ -14,11 +20,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String geofenceEvent = '';
+  StreamSubscription<GeofenceEvent>? geofenceEventStream;
+
   List<LatLng> tappedPoints = [];
   List<LatLng> circlePosition = [];
-
-  double _currentSliderValue = 200;
-
+  var latitude;
+  var longitude;
+  double _geoFenceRadius = 200;
+  StreamSubscription? _subscription;
   @override
   Widget build(BuildContext context) {
     final markers = tappedPoints.map((latlng) {
@@ -37,7 +47,7 @@ class _HomePageState extends State<HomePage> {
           borderStrokeWidth: 2,
           useRadiusInMeter: true,
           borderColor: Colors.blue,
-          radius: _currentSliderValue // 2000 meters | 2 km
+          radius: _geoFenceRadius // 2000 meters | 2 km
           );
     }).toList();
     // final markers = <Marker>[
@@ -57,6 +67,27 @@ class _HomePageState extends State<HomePage> {
           radius: 200 // 2000 meters | 2 km
           ),
     ];
+
+    void initGeoFenceService() async {
+      var permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission != LocationPermission.denied) {
+          print("permission for location granted");
+        } else {
+          print("Location Permission should be granted to use GeoFenceSerive");
+        }
+      } else {
+        print("permission for location granted");
+      }
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      initGeoFenceService();
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: Padding(
@@ -92,24 +123,50 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Slider(
-              value: _currentSliderValue,
+              value: _geoFenceRadius,
               max: 1000,
               divisions: 10,
-              label: _currentSliderValue.round().toString(),
+              label: _geoFenceRadius.round().toString(),
               onChanged: (double value) {
                 setState(() {
-                  _currentSliderValue = value;
+                  _geoFenceRadius = value;
                 });
               },
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Start Geo Fencing"),
-                ),
+                    child: const Text("Start Geo Fencing"),
+                    onPressed: () async {
+                      await Geofence.startGeofenceService(
+                          pointedLatitude: latitude.toString(),
+                          pointedLongitude: longitude.toString(),
+                          radiusMeter: _geoFenceRadius.toString(),
+                          eventPeriodInSeconds: 10);
+                      if (geofenceEventStream == null) {
+                        geofenceEventStream = Geofence.getGeofenceStream()
+                            ?.listen((GeofenceEvent event) {
+                          print(event.toString());
+                          if (event == GeofenceEvent.exit) {
+                            print(
+                                "------------------------------------out of geoFense");
+                          } else if (event == GeofenceEvent.enter) {
+                            print(
+                                "------------------------------------inside geoFense");
+                          }
+                          setState(() {
+                            print("-----------------updating geofence event");
+                            geofenceEvent = event.toString();
+                          });
+                        });
+                      }
+                    }),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Geofence.stopGeofenceService();
+                    geofenceEventStream?.cancel();
+                  },
                   child: const Text("Stop Geo Fencing"),
                 )
               ],
@@ -120,9 +177,16 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   void _handleTap(TapPosition tapPosition, LatLng latlng) {
     print("this is point location: $latlng");
     setState(() {
+      latitude = latlng.latitude;
+      longitude = latlng.longitude;
       tappedPoints.clear();
       circlePosition.clear();
       tappedPoints.add(latlng);
